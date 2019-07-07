59bf7a368a7c4b9eaa5196d9cf98bcf5e50807a0
League Student <league@WorthyMini-6.attlocal.net>
2019-07-07 14:27:16 -0700

    Houses

---
+++ src/_01_houses/Houses.java
@@ -0,0 +1,11 @@
+package _01_houses;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class Houses {
+public static void main(String[] args) {
+	Robot bob = new Robot();
+	bob.setX(50);
+	bob.setY(500);
+}
+}
