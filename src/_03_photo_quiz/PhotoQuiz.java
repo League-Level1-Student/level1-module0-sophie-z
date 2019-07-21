f1ecd17e51e74e7a45fdeca1a2ecfc098f0d2fc1
League Student <league@iMac-22.attlocal.net>
2019-07-21 14:27:48 -0700

    PhotoQuiz, DrumKit (IP)

--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -28,33 +28,42 @@ public class PhotoQuiz {
 		// “Copy Image Address”)
 String tennisracket = "https://images-na.ssl-images-amazon.com/images/I/51YcaRYpq4L._SX425_.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-
+Component holder = createImage(tennisracket);
 		// 3. use the "createImage()" method below to initialize your Component
-
 		// 4. add the image to the quiz window
-
+quizWindow.add(holder);
 		// 5. call the pack() method on the quiz window
-
+quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+String answerone = JOptionPane.showInputDialog("What kind of racket is this?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+if(answerone.equals("tennis")) {
+	System.out.println("CORRECT");
+}
 		// 8. print "INCORRECT" if the answer is wrong
-
+else {
+	System.out.println("INCORRECT");
+}
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+quizWindow.remove(holder);
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+String tennisballs = "https://www.gophersport.com/cmsstatic/img/117/G-51119-PennExtra-DutyChampionship-ce-11.jpg ";
+Component holdertwo = createImage(tennisballs);
 		// 11. add the second image to the quiz window
-
+quizWindow.add(holdertwo);
 		// 12. pack the quiz window
-
+quizWindow.pack();
 		// 13. ask another question
-
+String answertwo = JOptionPane.showInputDialog("What are these?");
 		// 14+ check answer, say if correct or incorrect, etc.
-
+if(answertwo.equals("tennis balls")) {
+	System.out.println("CORRECT");
+}
+else {
+	System.out.println("INCORRECT");
+}
 	}
 
 	private Component createImage(String imageUrl) throws MalformedURLException {
--- src/_04_drum_kit/DrumKit.java
+++ src/_04_drum_kit/DrumKit.java
@@ -25,35 +25,35 @@ public class DrumKit implements MouseListener {
 	public void run() throws MalformedURLException {
 
 		// 1. Make a JFrame variable and initialize it using "new JFrame()"
-
+JFrame f = new JFrame();
 		// 2. Make the frame visible and
 		// set its default close operation to JFrame.EXIT_ON_CLOSE
-
+f.setVisible(true);
 		// 3. Set the size of the frame
-
+f.setSize(500, 500);
 		// 4. Set the title of the frame
-
+f.setTitle("DrumKit");
 		// 5. Make a JPanel variable and initialize it using "new JPanel().
-
+JPanel p = new JPanel();
 		// 6. Add the panel to the frame. (The panel is invisible.)
-
+f.add(p);
 		// 7. Download an image of a drum from the Internet. Drop it into your
 		// Eclipse project under "default package".
 
 		// 8. Put the name of your image file in a String variable.
-
+String drumimage = "drum.jpg";
 		// 9. Edit the next line to use your String variable
 		// drumLabelWithImage = createLabelImage(drumImageString);
-
+drumLabelWithImage = createLabelImage("drum.jpg");
 		// 10. Add the image to the panel
-
+p.add(drumLabelWithImage);
 		// 11. Set the layout of the panel to "new GridLayout()"
-
+p.setLayout(new GridLayout());
 		// 12. call the pack() method on the frame. Run your program. Do you see
 		// your drum image?
-
+f.pack();
 		// 13. add this mouse listener to drumLabelWithImage
-
+drumLabelWithImage.addMouseListener(null);
 		// 18. Add more images to make a drumkit. Remember to add this mouse
 		// listener to each one.
 
@@ -62,7 +62,7 @@ public class DrumKit implements MouseListener {
 	public void mouseClicked(MouseEvent e) {
 		// 14. Print "mouse clicked" to the console. Run your program and watch
 		// the console to see when this is printed.
-
+System.out.println("mouse clicked");
 		JLabel drumClicked = (JLabel) e.getSource(); // This line gets the label
 														// that the mouse
 														// clicked on
